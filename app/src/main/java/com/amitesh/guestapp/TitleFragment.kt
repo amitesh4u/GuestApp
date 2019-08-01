d82e82e1ebfb5b16112fea85e05caf23296f6924
amitesh4u <amitesh4u@gmail.com>
2019-08-01 17:24:42 +0530

    Added Basic Title Fragment

---
+++ app/src/main/java/com/amitesh/guestapp/TitleFragment.kt
@@ -0,0 +1,31 @@
+package com.amitesh.guestapp
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.amitesh.guestapp.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Inflate using Data binding
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater, R.layout.fragment_title, container, false
+        )
+        return binding.root
+    }
+
+
+}
