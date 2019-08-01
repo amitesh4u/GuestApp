dbd37c266af6e666ba8544f63e81b8ffa9017d59
amitesh4u <amitesh4u@gmail.com>
2019-08-01 20:40:17 +0530

    Added all pages i.e. Fragments

---
+++ app/src/main/java/com/amitesh/guestapp/SmartKeyUsageFragment.kt
@@ -0,0 +1,36 @@
+package com.amitesh.guestapp
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.amitesh.guestapp.databinding.FragmentSmartKeyUsageBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SmartKeyUsageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Inflate using Data binding
+        val binding = DataBindingUtil.inflate<FragmentSmartKeyUsageBinding>(
+            inflater, R.layout.fragment_smart_key_usage, container, false
+        )
+        return binding.root
+    }
+
+
+}
